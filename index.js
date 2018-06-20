75c8f6e62b76a74f6e16c924fa596f5d79f48c8a
Luis Gonzalez <luisal1207@hotmail.es>
2018-06-20 13:46:15 -0600

    Adds suport for static files.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('This should be returned... <b>Cool, hu?</b>')
-})
 
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
